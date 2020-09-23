0e9079921fb5a2f210f448ca298915ae83535c25
liangyadong <17600665028@163.com>
2020-09-23 08:25:51 +0800

    Initial commit

---
+++ spring-cloud-base/src/main/java/com/xgcd/cloudbase/CloudbaseApplication.java
@@ -0,0 +1,13 @@
+package com.xgcd.cloudbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudbaseApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-base/src/test/java/com/xgcd/cloudbase/CloudbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgcd.cloudbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
