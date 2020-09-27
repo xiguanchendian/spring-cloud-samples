fe1f6009ea036cf323266499027a9eae9627c13d
liangyadong <liangyadong@stargraph.cn>
2020-09-27 11:13:01 +0800

    Eureka Client使用

--- spring-cloud-eureka-substitution1/src/main/java/com/xgcd/cloudeureka/rest/Substitution1Controller.java
+++ spring-cloud-eureka-substitution1/src/main/java/com/xgcd/cloudeureka/rest/Substitution1Controller.java
@@ -1,5 +1,7 @@
 package com.xgcd.cloudeureka.rest;
 
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,17 @@ public class Substitution1Controller {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private EurekaClient eurekaClient;
+
     @GetMapping("/callHello")
     public String callHello() {
         return restTemplate.getForObject("http://localhost:8081/service1/hello", String.class);// BeanConfiguration添加@LoadBalanced注解,此处修改为服务提供者名称报错
+    }
+
+    @GetMapping("/infos")
+    public Object siteName() {
+        // TODO security认证失败,用户名或密码错误
+        return eurekaClient.getInstancesByVipAddress("eureka-substitution1", false);
     }
 }
