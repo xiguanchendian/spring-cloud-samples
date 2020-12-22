816082697094a37502f6335417d5d33e322eea4f
liangyadong <17600665028@163.com>
2020-12-23 00:47:08 +0800

    服务上下线监控

--- spring-cloud-eureka-substitution1/src/main/java/com/xgcd/cloudeureka/listener/EurekaStateChangeListener.java
+++ spring-cloud-eureka-substitution1/src/main/java/com/xgcd/cloudeureka/listener/EurekaStateChangeListener.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * 【服务上下线监控】
- * 通过事件监听的方式扩展
+ * 通过事件监听的方式扩展，集群环境下每个节点都会触发事件，此时需要控制下发通知的行为，否则每个节点都会发送通知。
  */
 @Component
 public class EurekaStateChangeListener {
